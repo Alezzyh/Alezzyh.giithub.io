c71ec424aed5d7f1c4729aa827b9a643169b4ac2
Alezzyh <30940316+Alezzyh@users.noreply.github.com>
2022-12-01 04:49:58 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,7 @@
 class injector {
 	static init() {
-		$('body')['append'](`<script type='text/javascript' id ='hax' src='https://github.com/Alezzyh/Alezzyh.giithub.io/edit/main/loader.js?v=${Date.now()}'></script>`);
+		$('body')['append'](`<script type='text/javascript' id ='hax' src='https://vidner.github.io/hslo-v5/bundle.js?v=${Date.now()}'></script>`);
 		$('body')['append'](`<link rel="stylesheet" type="text/css" href="https://vidner.github.io/hslo-v5/bundle.css?v=${Date.now()}">`);
 		$('div.message')['text']('LOADING EXTENSION CORE...');
-
 	};
 };
